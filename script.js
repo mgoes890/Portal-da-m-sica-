5f18c05492a1f9e23866e28376be1f7e5b82c2ee
mgoes890 <00001125043714sp@al.educacao.sp.gov.br>
2025-09-08 21:56:44 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,4 @@
+// Quando a página carregar, exibe um alerta de boas-vindas
+document.addEventListener("DOMContentLoaded", function() {
+    alert("Bem-vindo ao Portal da Música! Explore Samba, Pagode e Funk.");
+});

e4b56c1bc3adc0924aebd2aaeb37931a4c674525
mgoes890 <00001125043714sp@al.educacao.sp.gov.br>
2025-09-09 14:37:53 -0300

    Update script.js

--- script.js
+++ script.js
@@ -2,3 +2,29 @@
 document.addEventListener("DOMContentLoaded", function() {
     alert("Bem-vindo ao Portal da Música! Explore Samba, Pagode e Funk.");
 });
+// Mensagem de boas-vindas
+document.addEventListener("DOMContentLoaded", function() {
+  alert("Bem-vindo ao Portal da Música! Explore Samba, Pagode e Funk.");
+});
+
+// Lógica do botão de acessibilidade
+const btn = document.getElementById("btnAcessibilidade");
+let contrasteAtivo = false;
+let textoMaior = false;
+
+btn.addEventListener("click", function() {
+  if (!contrasteAtivo && !textoMaior) {
+    document.body.classList.add("alto-contraste");
+    contrasteAtivo = true;
+    btn.textContent = "A++";
+  } else if (contrasteAtivo && !textoMaior) {
+    document.body.classList.add("texto-maior");
+    textoMaior = true;
+    btn.textContent = "Reset";
+  } else {
+    document.body.classList.remove("alto-contraste", "texto-maior");
+    contrasteAtivo = false;
+    textoMaior = false;
+    btn.textContent = "A+";
+  }
+});
